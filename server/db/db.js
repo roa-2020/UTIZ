1461b103a52aa834afd5a59659232c83038ea510
Anthony McGrath <akm@anthonykyle.co.nz>
2020-09-04 09:41:10 +1200

    Add Cuisines Route and API request

--- server/db/db.js
+++ server/db/db.js
@@ -1,16 +1,27 @@
 const request = require('superagent')
 const credentials = 'fc2f7c4167c36941daee1c0d9410fff2'
 const apiUrl = 'https://developers.zomato.com/api/v2.1/'
+const nzId = 148
 
 function getCategories () {
   return request
     .get(apiUrl + '/categories')
     .set('user-key', credentials)
     .set('Accept', 'application/json')
-    .then(categories => JSON.parse(categories.text))
+    .then(results => JSON.parse(results.text))
+    .catch(err => err)
+}
+
+function getCuisines (city_id) {
+  return request
+    .get(apiUrl + '/cuisines?city_id=' + city_id)
+    .set('user-key', credentials)
+    .set('Accept', 'application/json')
+    .then(results =>  results.body)
     .catch(err => err)
 }
 
 module.exports = {
-  getCategories
+  getCategories,
+  getCuisines
 }
--- server/routes/api.js
+++ server/routes/api.js
@@ -7,13 +7,40 @@ const router = express.Router()
 // put routes here
 router.get('/categories', (req, res) => {
   db.getCategories()
-    .then(categories => {
-      console.log(categories)
-      res.json(categories)
+    .then(results => {
+      console.log(results)
+      res.json(results)
     })
     .catch(err => {
       console.error(err)
       res.status(500).json({ message: 'Could not get categories.' })
+    })
+})
+
+router.get('/cuisines/', (req, res) => {
+  const id = 71
+  console.log('hello')
+  db.getCuisines(id)
+    .then(results => {
+      console.log(results)
+      res.json(results)
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({ message: 'Could not get cuisines.' })
+    })
+})
+
+router.get('/cuisines/:city_id', (req, res) => {
+  const id = req.params.city_id
+  db.getCuisines(id)
+    .then(results => {
+      console.log(results)
+      res.json(results)
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({ message: 'Could not get cuisines.' })
     })
 })
 
