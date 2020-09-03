30e5f8310044b7d181b2882d6118b3f8823024ed
Shrena Patel <shrena.p@hotmail.co.uk>
2020-09-04 10:36:13 +1200

    getCities api function done

--- client/api.js
+++ client/api.js
@@ -7,3 +7,9 @@ export function getCategories() {
     .then (response => response.body)
     .catch(err => err)
 }
+
+export function getCities(city_name = "") {
+    return request.get(baseURL + 'cities/' + city_name)
+    .then (response => response.body[0])
+    .catch(err => err)
+}
